05c1f9dfa21683dbbba404916f8fc3bc0dd0dbcf
Sam <samuelyerkes@gmail.com>
2014-01-09 09:42:22 -0500

    Fixes #4 - loads grunt replacements from separate .json file

--- gruntfile.js
+++ gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
       },
       css: {
         files: ['build/sass/*.sass'],
-        tasks: ['compass:dist', 'copy', 'replace']
+        tasks: ['compass:dist', 'copy', 'replace-css']
       }
     },
 
@@ -68,38 +68,7 @@ module.exports = function(grunt) {
       images: {
         src: ['t4/css/global.css'],
         overwrite: true,
-        replacements: [
-          {from: '../imgs/layout/background.jpg', to: '<t4 type="media" id="35702" formatter="path/*"/>'},
-          {from: '../imgs/layout/icons/icon-pdf.gif', to: '<t4 type="media" id="41389" formatter="path/*"/>'},
-          {from: '../imgs/layout/icons/icon-excel.gif', to: '<t4 type="media" id="41389" formatter="path/*"/>'},
-          {from: '../imgs/layout/icons/icon-word.gif', to: '<t4 type="media" id="41390" formatter="path/*"/>'},
-          {from: '../imgs/layout/li-bullet.gif', to: '<t4 type="media" id="35695" formatter="path/*"/>'},
-          {from: '../imgs/layout/li-bullet2.gif', to: '<t4 type="media" id="35694" formatter="path/*"/>'},
-          {from: '../imgs/layout/vcu-identity/body-bg.gif', to: '<t4 type="media" id="42666" formatter="path/*"/>'},
-          {from: '../imgs/layout/vcu-identity/vcubrand.gif', to: '<t4 type="media" id="42667" formatter="path/*"/>'},
-          {from: '../imgs/layout/header-gradient.jpg', to: '<t4 type="media" id="36474" formatter="path/*"/>'},
-          {from: '../imgs/layout/massey.png', to: '<t4 type="media" id="36476" formatter="path/*"/>'},
-          {from: '../imgs/layout/ncicc-link.gif', to: '<t4 type="media" id="35807" formatter="path/*"/>'},
-          {from: '../imgs/layout/search-bg.gif', to: '<t4 type="media" id="35701" formatter="path/*"/>'},
-          {from: '../imgs/layout/search-btn.gif', to: '<t4 type="media" id="35699" formatter="path/*"/>'},
-          {from: '../imgs/layout/nav-bar.png', to: '<t4 type="media" id="36545" formatter="path/*"/>'},
-          {from: '../imgs/layout/icons/quicklinks.png', to: '<t4 type="media" id="42989" formatter="path/*"/>'},
-          {from: '../imgs/layout/btn-submit.gif', to: '<t4 type="media" id="35693" formatter="path/*"/>'},
-          {from: '../imgs/layout/util-nav-icons.png', to: '<t4 type="media" id="35692" formatter="path/*"/>'},
-          {from: '../imgs/layout/footer.jpg', to: '<t4 type="media" id="36471" formatter="path/*"/>'},
-          {from: '../imgs/layout/footer-icons.png', to: '<t4 type="media" id="36477" formatter="path/*"/>'},
-          {from: '../imgs/layout/pencil.gif', to: '<t4 type="media" id="42708" formatter="path/*"/>'},
-          //from home.sass
-          {from: '../imgs/layout/home-news.png', to: '<t4 type="media" id="41588" formatter="path/*"/>'},
-          {from: '../imgs/layout/icon-calendar-bg.gif', to: '<t4 type="media" id="35675" formatter="path/*"/>'},
-          {from: '../imgs/layout/intro-nav.gif', to: '<t4 type="media" id="35665" formatter="path/*"/>'},
-          //from flexslider.sass
-          {from: '../imgs/layout/caption-bg.png', to: '<t4 type="media" id="36478" formatter="path/*"/>'},
-          {from: '../imgs/layout/slide-nav2.png', to: '<t4 type="media" id="42038" formatter="path/*"/>'},
-          {from: '../imgs/layout/intro-nav.png', to: '<t4 type="media" id="43159" formatter="path/*"/>'},
-         
-          //Continue adding images that are linked in the CSS here!
-        ]
+        replacements: []
       }
     },
     uncss: {
@@ -191,7 +160,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-sails-linker');
   grunt.loadNpmTasks('grunt-bower-task');
 
-  // Default task.
+grunt.registerTask('replace-css', function() {
+  var replacements = grunt.file.readJSON('replacements.json');
+  grunt.config('replace.images.replacements', replacements);
+  grunt.task.run('replace');
+});
+
+// Default task.
 grunt.registerTask('default', ['watch']);
 grunt.registerTask('build', ['bower', 'copy', 'sails-linker']);
 
