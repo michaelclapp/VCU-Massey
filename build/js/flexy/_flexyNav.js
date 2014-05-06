dabd4c50c61c8dba6c2c8a0b0afa17c50fae961a
Sam <samuelyerkes@gmail.com>
2014-05-06 10:02:14 -0400

    svg Massey logo, minor fixes to responsive nav

--- build/js/flexy/_flexyNav.js
+++ build/js/flexy/_flexyNav.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   
   var mainNav = $('#rufio nav');
-  var menuBtn = $('a#menu');
+  var menuBtn = $('a.menu');
+  var qlBtn = $('a.quicklinks');
   var tlLink = $('#rufio nav > ul li a');
   var tlLinkSpan = $('#rufio nav > ul li span a');
   var moreBtn = $('#rufio nav > ul li a.more');
@@ -16,17 +17,24 @@ $(document).ready(function() {
     $('#vcuIdentityWrapper, #rufio, #wrapper, #footer').toggleClass('active-nav');
 
   });
+
+  $(qlBtn).click(function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    $('#sidebar #quicklinks').toggleClass('active');
+    $('#vcuIdentityWrapper, #rufio, #wrapper, #footer').toggleClass('active-nav');
+  });
  
   //remove active-nav if body is clicked
   $('body:not(nav)').click(function() {
     if( $('#wrapper').hasClass('active-nav')){
       $('#vcuIdentityWrapper, #rufio, #wrapper, #footer').removeClass('active-nav');
-      $('#rufio nav, #rufio nav > ul').removeClass('active');
+      $('#rufio nav, #rufio nav > ul, #sidebar #quicklinks').removeClass('active');
       $('#rufio nav ul').addClass('off-left');
     }
   });
   
-  $('nav').click(function(e) {
+  $('nav, #sidebar #quicklinks').click(function(e) {
     e.stopPropagation();
   });
 
@@ -39,6 +47,8 @@ $(document).ready(function() {
   $('#rufio nav li:has(ul)').children('span').children('a').addClass('has-child');
 
   $('#rufio nav ul ul').prepend('<div class="menuControls"><a class="back pull-left" href="#"><i class="fa fa-chevron-left fa-2x"></i></a><a href="#" class="close pull-right"><i class="fa fa-times-circle-o fa-2x"></i></a></div>');
+
+  $('#sidebar #quicklinks').prepend('<div class="menuControls"><a href="#" class="close pull-right"><i class="fa fa-times-circle-o fa-2x"></i></a></div>');
 
   $('#rufio nav > ul').prepend('<div class="menuControls"><a href="#" class="close pull-right"><i class="fa fa-times-circle-o fa-2x"></i></a></div>');
 
@@ -55,27 +65,9 @@ $(document).ready(function() {
   $('a.close').click(function(e) {
     e.preventDefault();
     $(this).parent().parent().addClass('off-left');
-    $('#rufio nav, #rufio nav > ul').removeClass('active');
+    $('#rufio nav, #rufio nav > ul, #sidebar #quicklinks').removeClass('active');
     $('#vcuIdentityWrapper, #rufio, #wrapper, #footer').removeClass('active-nav');
     $('#rufio nav ul').addClass('off-left');
   });
-
-  /*var tlp = $('nav ul > li');
-    var tlpSpan = $('nav ul > li span')
-    
-    $(tlp).mouseover(function() {
-      $(this).children('ul').addClass('dip');
-    });
-    $(tlp).mouseout(function() {
-      $(this).children('ul').removeClass('dip');
-    });
-    $(tlpSpan).mouseover(function() {
-      $(this).children('ul').addClass('dip');
-      $(tlpSpan).addClass('dip');
-    });
-    $(tlpSpan).mouseout(function() {
-      $(this).children('ul').removeClass('dip');
-      $(tlpSpan).removeClass('dip');
-    }); */
 
 });
